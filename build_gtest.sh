100552c49ae6e33ec96eda7ec21daeb01075d00c
julienlopez <julien.lopez51@gmail.com>
2015-01-06 09:42:02 +0100

    trying to add a build with gcc4.8 for travis

--- build_gtest.sh
+++ build_gtest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$CXX" = "g++" ]; then export CXX="g++-4.8" CC="gcc-4.8"; fi
 cd /usr/src/gtest
-sudo cmake .
+sudo cmake . -DCXX="g++-4.8" -DCC="gcc-4.8"
 sudo make
 sudo mv libg* /usr/lib/
